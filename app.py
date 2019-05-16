33aad6958dcc80e5f4b6141ca0dd1d89515e0257
Panna Kristóf <kristofpanna@gmail.com>
2019-05-16 09:20:34 +0200

    Wednesday progress

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+counter = {'get_counter': 0, 'post_counter': 0}
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def route_index():
+    return render_template('index.html')
+
+
+@app.route('/request-counter', methods=['GET', 'POST'])
+def request_counter():
+    if request.method == 'GET':
+        counter['get_counter'] += 1
+
+    elif request.method == 'POST':
+        counter['post_counter'] += 1
+
+    return redirect('/')
 
 
 if __name__ == '__main__':
